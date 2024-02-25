0c7669e2a8713557c20fe9ea68893f7afef48ae9
Zaakir Mungrue <mungruez@gmail.com>
2024-02-25 04:44:15 -0400

    7

--- data/moves.js
+++ data/moves.js
@@ -4,6 +4,7 @@ export default [
         mid: 0,
         title: "Elbow Strike",
         vid: require("../assets/videos/elbowstrike.mp4"),
+        thumb:'../assets/thumbnails/elbowstrike.png',
         type:"Elbow",
         style:"Muay Tai"
     },
@@ -11,6 +12,7 @@ export default [
         mid: 1,
         title: "Hanuman",
         vid: require("../assets/video/hanuman.mp4"),
+        thumb:'../assets/thumbnails/hanuman.png',
         type:"Upper-Cut",
         style:"Muay Tai" 
     },
@@ -18,6 +20,7 @@ export default [
         mid: 2,
         title: "Achilles Leg Lock",
         vid: require("../assets/videos/achillesleglock.mp4"),
+        thumb:'../assets/thumbnails/achillesleglock.png',
         type:"Leg-Lock",
         style:"Muay Tai"
     },
@@ -25,6 +28,7 @@ export default [
         mid: 3,
         title: "Shoulder Lock",
         vid: require("../assets/video/shoulderlock.mp4"),
+        thumb:'../assets/thumbnails/shoulderlock.png',
         type:"Shoulder-Lock",
         style:"Muay Tai" 
     }
