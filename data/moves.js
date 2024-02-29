d785199724b51380de2ba154b5e3a8926fb22c4a
Zaakir Mungrue <mungruez@gmail.com>
2024-02-29 03:55:58 -0400

    s

--- data/moves.js
+++ data/moves.js
@@ -2,24 +2,6 @@
 export default [
     {
         mid: 0,
-        title: "Elbow Strike",
-        vid: '../assets/videos/elbowstrike.mp4',
-        thumb:'../assets/thumbnails/elbowstrike.png',
-        type:"Elbow",
-        style:"Muay Tai",
-        desc: "Basically it works like a point of a knife all the force is magnified by being concentrated in a small hard area. Even if the elbow doesn't deliver a knockout, it will almost always leave a nasty cut and often end the fight."
-    },
-    {
-        mid: 1,
-        title: "Hanuman",
-        vid: "../assets/videos/hanuman.mp4",
-        thumb:'../assets/thumbnails/hanuman.png',
-        type:"Upper-Cut",
-        style:"Muay Tai",
-        desc:" It's not the arms but the legs that drive our fists, like a piston the explosive power of the legs is capable of generating hundreds of pounds of force, all to a very vulnerable area, the soft underside of the opponent's jaw." 
-    },
-    {
-        mid: 2,
         title: "Achilles Leg Lock",
         vid: "../assets/videos/achillesleglock.mp4",
         thumb:'../assets/thumbnails/achillesleglock.png',
@@ -28,115 +10,7 @@ export default [
         desc:""
     },
     {
-        mid: 3,
-        title: "Shoulder Lock",
-        vid: "../assets/videos/shoulderlock.mp4",
-        thumb:'../assets/thumbnails/shoulderlock.png',
-        type:"Shoulder-Lock",
-        style:"Muay Tai",
-        desc:"" 
-    },
-    {
-        mid: 4,
-        title: "Palamut",
-        vid: "../assets/videos/palamut.mp4",
-        thumb:'../assets/thumbnails/palamut.png',
-        type:"Stun",
-        style:"Muay Tai",
-        desc:"By getting close, the opponent's neck is exposed, but because of the close range, the hand doesn't generate a huge amount of force. It's enough to stun and probably not enough to cause severe injury, but replace the hand with a weapon, a stick, or a knife as you would in combat and this could become a kill shot."
-    },
-    {
-        mid: 5,
-        title: "Seiken Choku Zuki",
-        vid: "../assets/videos/seikenchokuzuki.mp4",
-        thumb:'../assets/thumbnails/seikenchokuzuki.png',
-        type:"Punch",
-        style:"Muay Tai",
-        desc:"The key is rotating your fist one hundred and eighty degrees right before it strikes the intended target. The twisting snap of the arm maximize the speed of the punch and magnifies the force of the blow. By concentrating the impact on the first two knuckles of you hand. Done correctly it can deliver as much energy as a bowling ball dropped from the top of a house, more than enough to cause soft tissue damage, or break a rib." 
-    },
-    {
-        mid: 6,
-        title: "Kote Uchi",
-        vid: "../assets/videos/koteuchi.mp4",
-        thumb:'../assets/thumbnails/koteuchi.png',
-        type:"Hook",
-        style:"Muay Tai",
-        desc:"Because it swings through such a wide arc, the wrist is moving at speeds of up to fourty three miles per hour and since the radius doesn't deflect or compress nearly as much as a fist, it can deliver a staggering blow like being hit with an axe handle." 
-    },
-    {
-        mid: 7,
-        title: "Thpouk Kang Sdam",
-        vid: "../assets/videos/thpoukkangsdam.mp4",
-        thumb:'../assets/thumbnails/thpoukkangsdam.png',
-        type:"Punch",
-        style:"Muay Tai",
-        desc:"The rotation of the torso is the basis for the strikes power, because the torso has an effective mass far greater than the arms, its rotation can impact up to four hundred percent more force to your target, and the last minute rotation of the wrist can magnify that force even more." 
-    },
-    {
-        mid: 8,
-        title: "Double Elbow Slash",
-        vid: "../assets/videos/doubleelbowslash.mp4",
-        thumb:'../assets/thumbnails/doubleelbowslash.png',
-        type:"Elbow",
-        style:"Muay Tai",
-        desc:"The first elbow strike can accelerate to a speed of twenty five miles per hour in less than a quarter of a second. Less than a third of a second later, the initial strike is followed by another equally devastating shock. By employing the bony tip of the elbow, these strikes deliver a double dose of injury producing energy, and can result in a broken nose, facial fractures or a wicked cut." 
-    },
-    {
-        mid: 9,
-        title: "Neck Strike",
-        vid: "../assets/videos/neckstrike.mp4",
-        thumb:'../assets/thumbnails/neckstrike.png',
-        type:"Stun",
-        style:"Muay Tai",
-        desc:"Pulling off the move lies on two factors, the first is to combine the block and the strike into a single action. This reduces the time your opponent has to respond to just tenths of a second. But the real focus is to aim your strike at the throat. Because as little as seventy six pounds of force can collapse the larynx, the strike doesn't need to deliver a lot of force to be deadly. One blow like this and the fight is over." 
-    },
-    {
-        mid: 10,
-        title: "Power Angle Kick",
-        vid: "../assets/videos/poweranglekick.mp4",
-        thumb:'../assets/thumbnails/poweranglekick.png',
-        type:"Kick",
-        style:"Muay Tai",
-        desc:"The kicks power comes from the torque generated by the rotation of the hips. It's the same principle that cracks a whip, turning the hips just a few degrees forces the foot to travel a much greater distance, and creates a lot of speed as it does. The longer the leg and quicker the rotation the faster the kick, and unlike karate, my Tai fighters are also taught to strike with the hard bone of the shin. A My Thai kick can generate roughly the same force as a baseball bat, enough to crack a rib or a lake." 
-    },
-    {
-        mid: 11,
-        title: "Knee Strike",
-        vid: "../assets/videos/kneestrike.mp4",
-        thumb:'../assets/thumbnails/kneestrike.png',
-        type:"Knee",
-        style:"Muay Tai",
-        desc:"Getting enclosed, neutralizes the opponent's weapon advantage. Then, in the clinch, we bring the power of the knee to bear. Unlike the kick which relies on rotation to create power, the knee gets its power from leverage. Pulling our opponent down as we drive the knee up and deliver up to two point five tons of force. Enough to lift a car." 
-    },
-    {
-        mid: 12,
-        title: "Gedan Mawashi Geri",
-        vid: "../assets/videos/gedanmawashigeri.mp4",
-        thumb:'../assets/thumbnails/gedanmawashigeri.png',
-        type:"Kick",
-        style:"Muay Tai",
-        desc:"The kicks power comes from the snap of the knee. The snap is created when the quadricep, the largest muscle in the body, contracts and pulls the tendon connecting to the foot over the knee. Using the knee as a pulley, the strength of the quad is enough to accelerate your foot to speeds close to thirty miles per hour." 
-    },
-    {
-        mid: 13,
-        title: "Jodan Mawashi Geri",
-        vid: "../assets/videos/jodanmawashigeri.mp4",
-        thumb:'../assets/thumbnails/jodanmawashigeri.png',
-        type:"Kick",
-        style:"Muay Tai",
-        desc:"Because the area of impact is focused on a small part of the foot, it magnifies the force of the blow, making it capable of dropping one thousand pounds of force on your head. It's like getting whacked in the head with a Louisville Slugger." 
-    },
-    {
-        mid: 14,
-        title: "Fouette",
-        vid: "../assets/videos/fouette.mp4",
-        thumb:'../assets/thumbnails/fouette.png',
-        type:"Kick",
-        style:"Muay Tai",
-        desc:"This round house kick is designed for maximum impact. With the powerful muscles of the thigh yanking the tendon over the knee, the foot accelerates from zero to twenty one miles per hour in just one third of a second. Combine that with the reinforced shoe, and this kick can drive the nose bone back up into the brain." 
-    },
-    {
-        mid: 15,
+        mid: 1,
         title: "Chasse Bas",
         vid: "../assets/videos/chassebas.mp4",
         thumb:'../assets/thumbnails/chassebas.png',
@@ -145,16 +19,7 @@ export default [
         desc:"In this move, the forward propulsion of the hips, combined with the piston motion of the lower leg, can send well over seven hundred pounds of force directly through the rigid bone of the heel. This impact won't just knock an opponent down, the force is enough to permanently damage his kneecap." 
     },
     {
-        mid: 16,
-        title: "Gastrizein",
-        vid: "../assets/videos/gastrizein.mp4",
-        thumb:'../assets/thumbnails/gastrizein.png',
-        type:"Kick",
-        style:"Muay Tai",
-        desc:"Unlike most kicks that land with the side ball or top of the foot, the Gastrizen uses the heel to deliver the blow. This alignment of the heel, ankle and lower leg complex, channels some two thousand pounds of force into the opponent." 
-    },
-    {
-        mid: 17,
+        mid: 2,
         title: "Choke Defense",
         vid: "../assets/videos/chokedefense.mp4",
         thumb:'../assets/thumbnails/chokedefense.png',
@@ -163,7 +28,133 @@ export default [
         desc:"During the choke your opponent is relying on his grip strength and outstretched arms to maintain the choke. The strength of your back and shoulder muscles is greater than the applied force of your opponent's hold. So it doesn't take much to break his grip, and the closer your hands are to your own neck the greater the leverage." 
     },
     {
-        mid: 18,
+        mid: 3,
+        title: "Double Elbow Slash",
+        vid: "../assets/videos/doubleelbowslash.mp4",
+        thumb:'../assets/thumbnails/doubleelbowslash.png',
+        type:"Elbow",
+        style:"Muay Tai",
+        desc:"The first elbow strike can accelerate to a speed of twenty five miles per hour in less than a quarter of a second. Less than a third of a second later, the initial strike is followed by another equally devastating shock. By employing the bony tip of the elbow, these strikes deliver a double dose of injury producing energy, and can result in a broken nose, facial fractures or a wicked cut." 
+    },
+    {
+        mid: 4,
+        title: "Elbow Strike",
+        vid: '../assets/videos/elbowstrike.mp4',
+        thumb:'../assets/thumbnails/elbowstrike.png',
+        type:"Elbow",
+        style:"Muay Tai",
+        desc: "Basically it works like a point of a knife all the force is magnified by being concentrated in a small hard area. Even if the elbow doesn't deliver a knockout, it will almost always leave a nasty cut and often end the fight."
+    },
+    {
+        mid: 5,
+        title: "Fouette",
+        vid: "../assets/videos/fouette.mp4",
+        thumb:'../assets/thumbnails/fouette.png',
+        type:"Kick",
+        style:"Muay Tai",
+        desc:"This round house kick is designed for maximum impact. With the powerful muscles of the thigh yanking the tendon over the knee, the foot accelerates from zero to twenty one miles per hour in just one third of a second. Combine that with the reinforced shoe, and this kick can drive the nose bone back up into the brain." 
+    },
+    {
+        mid: 6,
+        title: "Gastrizein",
+        vid: "../assets/videos/gastrizein.mp4",
+        thumb:'../assets/thumbnails/gastrizein.png',
+        type:"Kick",
+        style:"Muay Tai",
+        desc:"Unlike most kicks that land with the side ball or top of the foot, the Gastrizen uses the heel to deliver the blow. This alignment of the heel, ankle and lower leg complex, channels some two thousand pounds of force into the opponent." 
+    },
+    {
+        mid: 7,
+        title: "Gedan Mawashi Geri",
+        vid: "../assets/videos/gedanmawashigeri.mp4",
+        thumb:'../assets/thumbnails/gedanmawashigeri.png',
+        type:"Kick",
+        style:"Muay Tai",
+        desc:"The kicks power comes from the snap of the knee. The snap is created when the quadricep, the largest muscle in the body, contracts and pulls the tendon connecting to the foot over the knee. Using the knee as a pulley, the strength of the quad is enough to accelerate your foot to speeds close to thirty miles per hour." 
+    },
+    {
+        mid: 8,
+        title: "Hanuman",
+        vid: "../assets/videos/hanuman.mp4",
+        thumb:'../assets/thumbnails/hanuman.png',
+        type:"Upper-Cut",
+        style:"Muay Tai",
+        desc:" It's not the arms but the legs that drive our fists, like a piston the explosive power of the legs is capable of generating hundreds of pounds of force, all to a very vulnerable area, the soft underside of the opponent's jaw." 
+    },
+    {
+        mid: 9,
+        title: "Jodan Mawashi Geri",
+        vid: "../assets/videos/jodanmawashigeri.mp4",
+        thumb:'../assets/thumbnails/jodanmawashigeri.png',
+        type:"Kick",
+        style:"Muay Tai",
+        desc:"Because the area of impact is focused on a small part of the foot, it magnifies the force of the blow, making it capable of dropping one thousand pounds of force on your head. It's like getting whacked in the head with a Louisville Slugger." 
+    },
+    {
+        mid: 10,
+        title: "Knee Strike",
+        vid: "../assets/videos/kneestrike.mp4",
+        thumb:'../assets/thumbnails/kneestrike.png',
+        type:"Knee",
+        style:"Muay Tai",
+        desc:"Getting enclosed, neutralizes the opponent's weapon advantage. Then, in the clinch, we bring the power of the knee to bear. Unlike the kick which relies on rotation to create power, the knee gets its power from leverage. Pulling our opponent down as we drive the knee up and deliver up to two point five tons of force. Enough to lift a car." 
+    },
+    {
+        mid: 11,
+        title: "Kote Uchi",
+        vid: "../assets/videos/koteuchi.mp4",
+        thumb:'../assets/thumbnails/koteuchi.png',
+        type:"Hook",
+        style:"Muay Tai",
+        desc:"Because it swings through such a wide arc, the wrist is moving at speeds of up to fourty three miles per hour and since the radius doesn't deflect or compress nearly as much as a fist, it can deliver a staggering blow like being hit with an axe handle." 
+    },
+    {
+        mid: 12,
+        title: "Neck Strike",
+        vid: "../assets/videos/neckstrike.mp4",
+        thumb:'../assets/thumbnails/neckstrike.png',
+        type:"Stun",
+        style:"Muay Tai",
+        desc:"Pulling off the move lies on two factors, the first is to combine the block and the strike into a single action. This reduces the time your opponent has to respond to just tenths of a second. But the real focus is to aim your strike at the throat. Because as little as seventy six pounds of force can collapse the larynx, the strike doesn't need to deliver a lot of force to be deadly. One blow like this and the fight is over." 
+    },
+    {
+        mid: 13,
+        title: "Palamut",
+        vid: "../assets/videos/palamut.mp4",
+        thumb:'../assets/thumbnails/palamut.png',
+        type:"Stun",
+        style:"Muay Tai",
+        desc:"By getting close, the opponent's neck is exposed, but because of the close range, the hand doesn't generate a huge amount of force. It's enough to stun and probably not enough to cause severe injury, but replace the hand with a weapon, a stick, or a knife as you would in combat and this could become a kill shot."
+    },
+    {
+        mid: 14,
+        title: "Power Angle Kick",
+        vid: "../assets/videos/poweranglekick.mp4",
+        thumb:'../assets/thumbnails/poweranglekick.png',
+        type:"Kick",
+        style:"Muay Tai",
+        desc:"The kicks power comes from the torque generated by the rotation of the hips. It's the same principle that cracks a whip, turning the hips just a few degrees forces the foot to travel a much greater distance, and creates a lot of speed as it does. The longer the leg and quicker the rotation the faster the kick, and unlike karate, my Tai fighters are also taught to strike with the hard bone of the shin. A My Thai kick can generate roughly the same force as a baseball bat, enough to crack a rib or a lake." 
+    },
+    {
+        mid: 15,
+        title: "Seiken Choku Zuki",
+        vid: "../assets/videos/seikenchokuzuki.mp4",
+        thumb:'../assets/thumbnails/seikenchokuzuki.png',
+        type:"Punch",
+        style:"Muay Tai",
+        desc:"The key is rotating your fist one hundred and eighty degrees right before it strikes the intended target. The twisting snap of the arm maximize the speed of the punch and magnifies the force of the blow. By concentrating the impact on the first two knuckles of you hand. Done correctly it can deliver as much energy as a bowling ball dropped from the top of a house, more than enough to cause soft tissue damage, or break a rib." 
+    },
+    {
+        mid: 16,
+        title: "Shoulder Lock",
+        vid: "../assets/videos/shoulderlock.mp4",
+        thumb:'../assets/thumbnails/shoulderlock.png',
+        type:"Shoulder-Lock",
+        style:"Muay Tai",
+        desc:"" 
+    },
+    {
+        mid: 17,
         title: "Spinning Back Kick",
         vid: "../assets/videos/spinningbackkick.mp4",
         thumb:'../assets/thumbnails/spinningbackkick.png',
@@ -172,10 +163,19 @@ export default [
         desc:"The power of this move comes from the spin. Like the wrecking ball at the end of a demolition crane, the power is generated by the rotation of the body. In conjunction with the snap of the knee, the foot achieves a velocity of nearly thirty miles per hour. When the heel connects with the solar plexus, it transfers fourteen hundred pounds of force. Enough to damage internal organs and fracture multiple ribs." 
     },
     {
+        mid: 18,
+        title: "Thpouk Kang Sdam",
+        vid: "../assets/videos/thpoukkangsdam.mp4",
+        thumb:'../assets/thumbnails/thpoukkangsdam.png',
+        type:"Punch",
+        style:"Muay Tai",
+        desc:"The rotation of the torso is the basis for the strikes power, because the torso has an effective mass far greater than the arms, its rotation can impact up to four hundred percent more force to your target, and the last minute rotation of the wrist can magnify that force even more." 
+    },
+    {
         mid: 19,
-        title: "",
-        vid: "../assets/videos/shoulderlock.mp4",
-        thumb:'../assets/thumbnails/shoulderlock.png',
+        title: "Uchi Uke",
+        vid: "../assets/videos/uchiuke.mp4",
+        thumb:'../assets/thumbnails/uchiuke.png',
         type:"Kick",
         style:"Muay Tai",
         desc:"" 
