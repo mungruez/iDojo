e97c0767513f4b96380601302fc8b2232cec4aab
Zaakir Mungrue <mungruez@gmail.com>
2026-06-25 02:12:20 -0400

    resmargins

--- screens/Resources.js
+++ screens/Resources.js
@@ -46,29 +46,29 @@ export default function Resources() {
 <Text style={ styles.title }>Years of research into accumulating the best video, audio and graphics for Self Defense Moves has made iDojo a masterpice. All future upgrades will be free as the main goal is to teach Self Defense to those who would use it only when required. Use the volume buttom(top right) to control sounds, videos can be played in slow motion, wifi is only needed for the Featured List. DojoSoft`s continues to innovate by placing an invisible button in this iDojo App that will launch a secret password Manager App. DojoSoft promises to be the best Self Defense mobile App by releasing meaningful upgrades in the future. We plan to realse:-  In-app video recording and move analysis to allow users to record themselves performing techniques and use a video analysis service to provide feedback on their form, timing, and execution to offer personalized coaching tips based on the user's performance. Other future upgrades include:-  A community forum, Challenges and an AI Coach for traing and gear.
 </Text>
 
-<Text style={ styles.movesList }>Moves List: </Text>
+<Text style={ styles.movesList }>Moves List:</Text>
 <Text style={ styles.title }> A list of over eighty videos of Moves with audio and text instructions. Each video was carefully edited to contain a description with an AI Morpheus voice and can be slowed in order to, view the Move in slow motion.</Text>
 
-<Text style={ styles.manuals }>Manuals: </Text>
+<Text style={ styles.manuals }>Manuals:</Text>
 <Text style={ styles.title }>A list of over 100 Moves for Self Defense training. Each Move has step by step instructions with images to make learning Moves easier. 
 </Text>
 
-<Text style={ styles.addMove }>Add Move: </Text>
+<Text style={ styles.addMove }>Add Move:</Text>
 <Text style={ styles.title }> Add, Share, Edit, View, Delete and Import your own Self Defense Moves into the iDojo App. You can also share the Move image or images, a single video or a single pdf when viewing a move. Moves can only be shared and imported with the iDojo App, only single videos, images and PDFs can be shared externally and instuctions are provided. Awesome for sharing individual Self Defence stories, albums, events and more. </Text>
 
-<Text style={ styles.chapters }>Chapters: </Text>
+<Text style={ styles.chapters }>Chapters:</Text>
 <Text style={ styles.title }>Add, Share, Edit, View, Delete and Import your own Chapters to the iDojo App. A Chapter is a collection of videos, audios, images and PDFs in any number and in any order. You can also share an individual Chapter image, a single video or a single pdf when viewing a chapter. Chapters can be shared and imported with the iDojo App and our free wheeShare App. Only single videos, images and PDFs can be shared externally and instuctions are provided. Chapters are awesome for sharing lessons, courses, albums, events and more.
 </Text>
 
-<Text style={ styles.fightersList }>Fighters List: </Text>
+<Text style={ styles.fightersList }>Fighters List:</Text>
 <Text style={ styles.title }> A first time ever, list of the best fighters of all time. Each fighter was hand picked and carefully researched by DojoSoft before being added to the list. DojoSoft only considers real life, fighting styles that are practised for effective Self Defense. 
 </Text>
 
-<Text style={ styles.featured }>Featured: </Text>
+<Text style={ styles.featured }>Featured:</Text>
 <Text style={ styles.title }>DojoSoft's hand picked list of online videos and shorts for Self Defense, with our Featured Move Of The Day. Whether your looking for one to one paid training or just a quick watch and learn, then, this iDojo's Featured List has it. Each video must meet certain criteria in order to be considered effective Self Defense Training. WiFi is necessary for this section as Featured videos are copyrighted. Please contact us on social media to have your suggested videos in our Featured List.
 </Text>
 
-<Text style={ styles.freeyourmind }>Free Your Mind: </Text>
+<Text style={ styles.freeyourmind }>Free Your Mind:</Text>
 <Text style={ styles.title }>Audio for a healthy mind tthat will motivate Self Defense learning and intense training. Each Audio is carefully handcrafted to be interlectually, heart touching to enable you to free your mind. When playing audio, click the purple bar in the slider for seeking.
           </Text>
         </View>
@@ -95,7 +95,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(211, 211, 211, 0.1)',
     marginLeft:19,
     marginRight:19,
-    marginTop:12,
+    marginTop: 7,
+    marginBottom: 12,
     padding: 5,
   },
   text: {
