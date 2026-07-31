422d45442aefe1954c1b7902d6d3e43b432c6613
Zaakir Mungrue <mungruez@gmail.com>
2026-07-31 18:58:28 -0400

    test

--- data/fighters.js
+++ data/fighters.js
@@ -182,13 +182,13 @@ export const fighters = [
         {
           id: "82",
           title: "Pacman Countwr Punch - Part 1",
-          img: require('../assets/fighters/pacmancounterpunchgf.png'),
+          img: require('../assets/fighters/pacmancounterpunchgf.jpg'),
           desc: "Pacquiao utilizes a combination of head movement, footwork, and blocking techniques to avoid incoming punches. Pacquiao's ability to slip punches and counterattack has frustrated many of his opponents, allowing him to maintain control of the fight. Here he uses a parry to knock the opponents incomming punch down and immmediately prepare for a counter punch. His defensive prowess, combined with his offensive capabilities, has made him a well-rounded and formidable boxer. By placing his lead foot infront of and between his opponent's feet, Pac Man is able to create better angles for defense and for punches and hooks.",
         },
         {
           id: "83",
           title: "Pacman Countwr Punch - Part 2",
-          img: require('../assets/fighters/pacmancounterpunch2gf.png'),
+          img: require('../assets/fighters/pacmancounterpunch2gf.jpg'),
           desc: "After successfully parrying his opponent's punch, Pacquiao quickly shifts his weight and rotates his hips to generate power for his counterpunch. This movement allows him to deliver a strong and accurate punch while maintaining his balance and positioning in the ring. Pacquiao's ability to seamlessly transition from defense to offense is a testament to his skill and experience as a boxer.",
         },
         {
